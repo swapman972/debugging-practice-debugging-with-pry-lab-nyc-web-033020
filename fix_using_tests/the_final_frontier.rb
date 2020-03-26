3df5a8934039f4150cba570ae0587a011c3c87df
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-26 17:54:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -13,8 +13,8 @@ def crew
   ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
 end
 
-def greet_crew (crew)
-  crew.each do 
+def greet_crew (list)
+  list.each do 
     |n| "Hello " + n +"."
   end
 end
