e0c6d326a287bfd106a3b4d3cf969c474a840529
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-03-26 17:39:24 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -13,6 +15,7 @@ end
 
 def greet_crew (crew)
   crew.each {|crew_member| "Hello #{crew_member}."}
+  binding.pry
 end
 
 def engage
